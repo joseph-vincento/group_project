0ec463fd14adcdab5cdfb8608447dbbd6ecf59ef
brv18004 <brv18004@brv18004-vm.cse.uconn.edu>
2023-10-23 10:55:09 -0400

    Working backend that connects current html files

---
+++ src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping({"/welcome_page", "/"})
+    public String welcomePage() {
+        return "welcome_page";
+    }
+
+
+
+    // Endpoint for the login (or quiz home) page
+    @GetMapping("/quiz_home")
+    public String quizHome() {
+        return "quiz_home"; // This should match the name of your quiz_home.html
+    }
+
+    // Endpoint for the sign-up page
+    @GetMapping("/signUp")
+    public String signUp() {
+        return "signUp"; // This should match the name of your signUp.html
+    }
+
+}
